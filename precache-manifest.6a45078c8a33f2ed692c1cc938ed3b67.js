9a006e5c7df7a33c361ca148a85bdbd5917b247f
prasastiatia <tias1508@gmail.com>
2020-03-29 17:14:09 +0700

    Updates

---
+++ precache-manifest.6a45078c8a33f2ed692c1cc938ed3b67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e01e410956e7701a198de8ffc067d05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bef23509284e71de496",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cb70a67cffec224e160b",
+    "url": "/static/js/2.de8053c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de8053c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bef23509284e71de496",
+    "url": "/static/js/main.d052d27a.chunk.js"
+  },
+  {
+    "revision": "824a87c525fc41ffa25d",
+    "url": "/static/js/runtime-main.078ffda6.js"
+  }
+]);
