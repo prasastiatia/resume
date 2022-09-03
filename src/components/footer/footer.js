f70711333223a4b2200a0cb3b8c2cc2182f0754a
prasastiatia <tias1508@gmail.com>
2022-09-03 18:48:28 +0700

    update portfolio view

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
-    let resumeData = this.props.resumeData;
+    
     return (
       <React.Fragment>
       {/*generated code*/}
